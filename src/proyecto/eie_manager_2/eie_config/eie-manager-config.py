bbb5d77026b2cb952bb8a5acadbecddb19084865
Dualok <djfonsecamo@gmail.com>
2022-07-18 20:21:40 -0600

    thing-policy requests

--- src/proyecto/eie_manager_2/eie_config/eie-manager-config.py
+++ src/proyecto/eie_manager_2/eie_config/eie-manager-config.py
@@ -4,8 +4,8 @@ import json
 import time
 import struct
 
-url = "http://localhost:8080/api/2/policies/my.test:policy"
-
+url_policy = "http://localhost:8080/api/2/policies/my.test:policy"
+url_thing = "http://localhost:8080/api/2/things/my.test:octopus"
 
 def encode_json(file):
     f = open(file)
@@ -19,13 +19,21 @@ def encode_json(file):
     return payload
 
 
-def connection_config():
+#def connection_config():
 
 
-def ditto_register():
-    payload = encode_json("payload.json")
+def ditto_policy_register(url):
+    payload = encode_json("policy.json")
     print(payload)
     r = requests.put(url, data = payload)
 
 
-ditto_register()
+def ditto_thing_register(url):
+    payload = encode_json("thing.json")
+    print(payload)
+    r = requests.put(url, data = payload)
+
+
+ditto_thing_register(url_thing)
+time.sleep(5)
+ditto_policy_register(url_policy)
