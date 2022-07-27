22bc6822da82ec8b06e00a7246806a246c53eba5
Dualok <djfonsecamo@gmail.com>
2022-07-27 02:07:21 -0600

    eie-manager-config 3 containers

--- src/proyecto/eie_manager_2/eie_config/eie-manager-config.py
+++ src/proyecto/eie_manager_2/eie_config/eie-manager-config.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python3
+from flask import Flask, render_template, request
 import requests
-import paho.mqtt.client as mqtt
 import json
 import time
 import struct
 
+
 url_policy = "http://localhost:8080/api/2/policies/my.test:policy"
 url_thing = "http://localhost:8080/api/2/things/my.test:octopus"
+app = Flask(__name__)
+
 
 def encode_json(file):
     f = open(file)
-    # returns JSON object as
+    #returns JSON object as
     # a dictionary
     payload = json.load(f)
     # Closing file
@@ -19,21 +23,60 @@ def encode_json(file):
     return payload
 
 
-#def connection_config():
+def decode_json(payload):
+    payload_decoded = payload.decode('utf8').replace("'", '"')
+    payload_decoded =   json.loads(payload_decoded)
+    payload_decoded = json.dumps(payload_decoded)
+    return payload_decoded;
 
 
 def ditto_policy_register(url):
     payload = encode_json("policy.json")
-    print(payload)
-    r = requests.put(url, data = payload)
+    payload_decoded = decode_json(payload)
+    print("Unserialized payload\n", payload_decoded)
+    print('Serialized payload', payload)
+    try:
+        #r = requests.put(url, data = payload)
+        output = 'Sending' + payload_decoded
+        print(output)
+        return output
+    except:
+        return 'Port not available'
 
 
 def ditto_thing_register(url):
     payload = encode_json("thing.json")
-    print(payload)
-    r = requests.put(url, data = payload)
+    payload_decoded = decode_json(payload)
+    print("Unserialized payload\n", payload_decoded)
+    print('Serialized payload', payload)
+    try:
+        #r = requests.put(url, data = payload)
+        output = 'Sending' + payload_decoded
+        return output
+    except:
+        return 'Port not available'
 
 
-ditto_thing_register(url_thing)
-time.sleep(5)
-ditto_policy_register(url_policy)
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+
+@app.route('/request', methods = ['POST', 'GET'])
+def send_request():
+    if request.form.get('Policy')== 'Policy':
+        return ditto_policy_register(url_policy)
+        time.sleep(2)
+    elif request.form.get('Thing') == 'Thing':
+        return ditto_thing_register(url_thing)
+        time.sleep(2)
+    #data = ditto_policy_register(url_policy)
+    #return render_template('index.html', data = data)
+
+
+if __name__ == '__main__':
+    app.run()
+
+#time.sleep(5)
+#ditto_policy_register(url_policy)
+#print(ditto_policy_register(url_policy))
